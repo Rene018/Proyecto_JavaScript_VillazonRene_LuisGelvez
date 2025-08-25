e294c83b58913d2b999c5be291c30ecacefc6f44
Rene018 <rene18agosto@gmail.com>
2025-08-25 14:58:45 -0500

    feat: ✨ Implement login validation and dashboard data fetching

---
+++ scripts/mainDash.js
@@ -0,0 +1,46 @@
+async function fetchCursosActivos(enp, cont) {
+  const url = new URL(enp);
+  url.searchParams.append("estado", "Activo");
+  fetch(url, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.length);
+      document.getElementById(cont).textContent = data.length;
+    });
+}
+async function cargarAll() {
+  await fetchCursosActivos(
+    "https://68abc0057a0bbe92cbb82d40.mockapi.io/lms/cursos",
+    "cant-cursos"
+  );
+  await fetchCursosActivos(
+    "https://68abc0057a0bbe92cbb82d40.mockapi.io/lms/users?tipo=Docente",
+    "cant-docentes"
+  );
+  await fetchCursosActivos(
+    "https://68abc0057a0bbe92cbb82d40.mockapi.io/lms/users?tipo=Estudiante",
+    "cant-estudiantes"
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+  document
+    .querySelectorAll(".dropdown-name")
+    .forEach((el) => (el.textContent = usuario["nombre"]));
+  document
+    .querySelectorAll(".dropdown-correo")
+    .forEach((el) => (el.textContent = usuario["correo"]));
+
+  document.getElementById("btnLogout").addEventListener("click", function () {
+    localStorage.removeItem("usuario");
+    window.location.href = "../../index.html";
+  });
+
+  //===============================000===============================//
+  cargarAll()
+});
